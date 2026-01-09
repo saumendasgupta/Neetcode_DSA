aed969d502dc35ed486e9e94d287e104b1eb3e0f
Saumen Dasgupta <saumendasgupta@gmail.com>
2026-01-09 19:10:33 +0530

    feat: Create Longest_Consecutive_Sequence.java

---
+++ Longest_Consecutive_Sequence.java
@@ -0,0 +1,44 @@
+/************
+  Given an array of integers nums, return the length of the longest consecutive sequence of elements that can be formed.
+
+A consecutive sequence is a sequence of elements in which each element is exactly 1 greater than the previous element. The elements do not have to be consecutive in the original array.
+
+You must write an algorithm that runs in O(n) time.
+
+Example 1:
+
+Input: nums = [2,20,4,10,3,4,5]
+
+Output: 4
+Explanation: The longest consecutive sequence is [2, 3, 4, 5].
+
+Example 2:
+
+Input: nums = [0,3,2,5,4,6,1,1]
+
+Output: 7
+  ****************************/
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> set = new HashSet();
+        int maxLength=0;
+        int currentLength = 0;
+        for(int num:nums){
+            set.add(num);
+        }
+        for(int num:set){
+            if(!set.contains(num-1)){
+                int currentNumber = num;
+                int currentSteak = 1;
+                while(set.contains(currentNumber+1)){
+                    currentSteak++;
+                    currentNumber++;
+                }
+                maxLength = Math.max(maxLength,currentSteak);
+                
+            }
+        }
+        
+        return maxLength;
+    }
+}
