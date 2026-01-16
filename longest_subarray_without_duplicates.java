ea56fb4550460c1c4121b47ebac29612e3ca64f8
Saumen Dasgupta <saumendasgupta@gmail.com>
2026-01-16 16:02:31 +0530

    feat: Create longest_subarray_without_duplicates.java

---
+++ longest_subarray_without_duplicates.java
@@ -0,0 +1,25 @@
+/*************
+  Given a string s, find the length of the longest substring without duplicate characters.
+
+A substring is a contiguous sequence of characters within a string.
+
+Example 1:
+
+Input: s = "zxyzxyz"
+
+Output: 3
+  *****************/
+public int lengthOfLongestSubstring(String s) {
+        int left=0;
+        HashSet<Character> set = new HashSet();
+        int maxLength=0;
+        int currentLength=0;
+        for(int right=0;right<s.length();right++){
+            while(set.contains(s.charAt(right))){
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(right));
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+        return maxLength;
