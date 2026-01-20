454f0425cac7e931f269d6f1f4cb66812905626c
Saumen Dasgupta <saumendasgupta@gmail.com>
2026-01-20 10:19:12 +0530

    feat: Create reorder_list.java

---
+++ reorder_list.java
@@ -0,0 +1,76 @@
+/*******
+You are given the head of a singly linked-list.
+
+The positions of a linked list of length = 7 for example, can intially be represented as:
+
+[0, 1, 2, 3, 4, 5, 6]
+
+Reorder the nodes of the linked list to be in the following order:
+
+[0, 6, 1, 5, 2, 4, 3]
+
+Notice that in the general case for a list of length = n the nodes are reordered to be in the following order:
+
+[0, n-1, 1, n-2, 2, n-3, ...]
+
+You may not modify the values in the list's nodes, but instead you must reorder the nodes themselves.
+
+Example 1:
+
+Input: head = [2,4,6,8]
+
+Output: [2,8,4,6]
+Example 2:
+
+Input: head = [2,4,6,8,10]
+
+Output: [2,10,4,8,6] 
+**************/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public void reorderList(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            fast=fast.next.next;
+            slow = slow.next;
+        }
+        //we got the middle of the list
+
+        ListNode curr = slow.next;
+        slow.next=null;
+        ListNode prev=null;
+        ListNode next = null;
+        while(curr!=null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        ListNode mid = prev;
+        fast = head;
+        
+        ListNode fastTemp ;
+        ListNode midTemp ;
+        while(mid!=null){
+            fastTemp = fast.next;
+            midTemp = mid.next;
+            fast.next = mid;
+            mid.next=fastTemp;
+            mid = midTemp;
+            fast=fastTemp;
+        }
+
+    }
+}
